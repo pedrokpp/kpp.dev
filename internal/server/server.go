6cb52812a4caa2bdb4d60f9b19bc0985b12d8082
pedrokpp <pedrokp@id.uff.br>
2024-01-20 23:05:39 -0300

    refactor: serving static files instead of template

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,18 +1,14 @@
 package server
 
 import (
-	"html/template"
-
 	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		tmpl, _ := template.ParseFiles("./templates/index.html")
-		tmpl.Execute(c.Writer, nil)
-	})
+	r.StaticFile("/", "./public/index.html")
+	r.StaticFile("/style.css", "./public/style.css")
 
 	return r
 }
